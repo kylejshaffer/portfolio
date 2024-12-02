28dd95cb1bd9e948c5a78a77aa5acf1240a91799
Kyle Shaffer <kshaffer@sdl.com>
2024-12-02 12:50:32 -0800

    updating with latest web project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import twitterapp from "../assets/twitter-app.png";
 import llmapp from "../assets/llm-app.png";
 import bandpage from "../assets/band-page.png";
+import climateapp from "../assets/climate-app.png";
 import { Card, CardBody, CardFooter, Divider, Button, Image, Stack, Heading, Text } from "@chakra-ui/react";
 import { SimpleGrid, Link, VStack } from '@chakra-ui/react';
 
@@ -36,12 +37,11 @@ function ProjectCard({ heading, description, tools, screenshot, link }) {
 
 function Projects() {
     const projData = [{
-        heading: "Twitter Covid Sentiment Data App",
-        description: "Data app visualizing sentiment expressed on Twitter during COVID-19.",
+        heading: "Climate Dashboard",
+        description: "Web app visualizing temperature and precipitation from 2000-24 in the U.S.",
         tools: "D3.js, JavaScript, CSS, HTML",
-        link: "https://covid-sentiment-viz.vercel.app/",
-        screenshot: twitterapp,
-
+        link: "https://climate-dashboard-phi.vercel.app/",
+        screenshot: climateapp,
     },
     {
         heading: "Visual Question Answering AI App",
@@ -49,6 +49,14 @@ function Projects() {
         tools: "React, JavaScript, REST APIs, HuggingFace",
         link: "https://vqa-llm-app.vercel.app/",
         screenshot: llmapp,
+
+    },
+    {
+        heading: "Twitter Covid Sentiment Data App",
+        description: "Data app visualizing sentiment expressed on Twitter during COVID-19.",
+        tools: "D3.js, JavaScript, CSS, HTML",
+        link: "https://covid-sentiment-viz.vercel.app/",
+        screenshot: twitterapp,
 
     },
     {
@@ -69,7 +77,7 @@ function Projects() {
             <Link href="https://www.chakra-ui.com/" target="_blank" rel="noreferrer">ChakraUI</Link> to more specialized data visualization libraries
             like <Link href="https://d3js.org/" target="_blank" rel="noreferrer">D3.js</Link>, and I'm excited to learn more.
         </Text>
-        <SimpleGrid columns={{sm: 2, md: 3}} spacing={4}>
+        <SimpleGrid columns={{sm: 2, md: 2}} spacing={4}>
             {projData.map(d => {
                 return <ProjectCard
                 heading={d.heading}
